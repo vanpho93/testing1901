2abfea3fe44c78557f80b2b92a0ebaf27377d089
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-21 17:49:00 +0700

    test with promise

--- test/addPromise.test.js
+++ test/addPromise.test.js
@@ -2,5 +2,19 @@ const assert = require('assert');
 const { addPromise } = require('../src/addPromise');
 
 describe('Test addPromise function', () => {
-    
+    it('Can add 2 numbers', done => {
+        addPromise(4, 5)
+        .then(result => {
+            assert.equal(result, 9);
+            done();
+        });
+    });
+
+    it('Cannot add a number and a string', done => {
+        addPromise('x', 5)
+        .catch(error => {
+            assert.equal(error.message, 'Type error');
+            done();
+        });
+    });
 });
